db3dc0711ff910fc6660b3ccdd31040af2a17a20
Yan, Jiajun <1466649452@qq.com>
2024-10-20 13:54:37 +0800

    Initial Commit

---
+++ LaTeXOCR/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LaTeXOCR
+//
+//  Created by 颜家俊 on 2024/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LaTeXOCR/LaTeXOCRApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaTeXOCRApp.swift
+//  LaTeXOCR
+//
+//  Created by 颜家俊 on 2024/10/20.
+//
+
+import SwiftUI
+
+@main
+struct LaTeXOCRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
