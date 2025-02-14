de870ae97211a9186b64507d70aa3bc734f27c3a
Yan, Jiajun <1466649452@qq.com>
2025-02-14 14:27:23 +0800

    添加状态栏候选功能列表

---
+++ LaTeXOCR/AppDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  AppDelegate.swift
+//  LaTeXOCR
+//
+//  Created by 颜家俊 on 2024/11/5.
+//
+
+import Foundation
---
+++ LaTeXOCR/mouse.swift
@@ -0,0 +1,18 @@
+//
+//  mouse.swift
+//  LaTeXOCR
+//
+//  Created by 颜家俊 on 2024/10/20.
+//
+
+import Cocoa
+
+class mouse: NSView {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
